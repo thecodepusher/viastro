4b0404641667856296792854316a06db7475d489
Dragan Krivokuca <thecodepusher@gmail.com>
2025-11-24 22:12:18 +0100

    🐛 fix: vercel config

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,9 +4,4 @@ import type { Config } from "@react-router/dev/config";
 export default {
   ssr: true,
   presets: [vercelPreset()],
-  future: {
-    v3_fetcherPersist: true,
-    v3_relativeSplatPath: true,
-    v3_throwAbortReason: true,
-  },
 } satisfies Config;
